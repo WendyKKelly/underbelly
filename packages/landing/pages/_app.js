187f743154c55e6233d4498b2aff3efadf8eb8f5
Wendy Kelly <wendykelly@Wendys-iMac.local>
2020-08-04 21:08:01 -0700

    added a bit of a fix for the links not working

--- packages/landing/pages/_app.js
+++ packages/landing/pages/_app.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Modal } from '@redq/reuse-modal';
 import '@redq/reuse-modal/es/index.css';
 import 'rc-tabs/assets/index.css';
+import 'common/src/assets/css/empty.scss';
 
 export default ({ Component, pageProps }) => {
   return (
